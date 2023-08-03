4c1de86b80984352dfa10d2d9197f919ed8c43a9
Nick Galante <nicholasgalante1997@gmail.com>
2023-08-03 10:20:35 -0600

    writes to table

--- api/shelf/create.ts
+++ api/shelf/create.ts
@@ -1,4 +1,5 @@
 import { geolocation, ipAddress } from '@vercel/edge';
+import { sql } from '@vercel/postgres';
 import { HmacSHA256, enc } from 'crypto-js';
 
 export const config = {
@@ -9,6 +10,7 @@ export default async function handler(request: Request) {
   const urlParams = new URL(request.url).searchParams;
   const query = Object.fromEntries(urlParams);
   const uuid = query.uuid;
+  const tag = query.tag ?? 'no-tag';
   if (!uuid) {
     return new Response(
         JSON.stringify({ 
@@ -21,7 +23,7 @@ export default async function handler(request: Request) {
             statusText: 'ServerException',
             headers: {
                 'content-type': 'application/json',
-                'x-edge-token': process.env.VERCEL_EDGE_TOKEN
+                'x-edge-token': process.env.VERCEL_EDGE_HEADER_TOKEN ?? ''
             }
         }
     )
@@ -36,32 +38,55 @@ export default async function handler(request: Request) {
             ok: false,
             data: null,
             error: 'VercelEdgeFunctionException::MissingIPAddress'
-        })
+        }),
+        {
+            status: 500,
+            statusText: 'ServerException',
+            headers: {
+                'content-type': 'application/json',
+                'x-edge-token': process.env.VERCEL_EDGE_HEADER_TOKEN ?? ''
+            }
+        }
     );
   }
 
   const message = JSON.stringify([uuid, latitude, longitude, country, countryRegion, city, region, ip]);
   const key = process.env.VERCEL_HMAC_KEY;
 
+  if (!key) {
+    return new Response(
+        JSON.stringify({
+            ok: false,
+            data: null,
+            error: 'VercelEdgeFunctionException::MissingEnvKey'
+        }),
+        {
+            status: 500,
+            statusText: 'ServerException',
+            headers: {
+                'content-type': 'application/json',
+                'x-edge-token': process.env.VERCEL_EDGE_HEADER_TOKEN ?? ''
+            }
+        }
+    );
+  }
+
   const shelfKey = enc.Hex.stringify(HmacSHA256(message, key));
-  console.log(shelfKey);
+
+  const { command, fields, oid, rowCount, rows, } = await sql`insert into shelves(uuid, shelfkey, longitude, latitude, ip_address, tag, country, country_region, region) values(${uuid}, ${shelfKey}, ${longitude}, ${latitude}, ${ip}, ${tag}, ${country}, ${countryRegion}, ${region});`;
+  console.log(`SQL Query Response`);
+  console.log({ command, fields, oid, rowCount, rows });
 
   return new Response(
     JSON.stringify({
-        latitude,
-        longitude,
-        countryRegion,
-        country,
-        region,
-        city,
-        ipAddress: ip,
+        uuid,
         shelfKey
     }),
     {
       status: 201,
       headers: {
         'content-type': 'application/json',
-        'x-edge-token': process.env.VERCEL_EDGE_HEADER_TOKEN
+        'x-edge-token': process.env.VERCEL_EDGE_HEADER_TOKEN ?? ''
       }
     }
   );
